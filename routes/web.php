702ab662509d499b029ccdd4e5bf72161a120eb7
ronaldemanuel <emronald15el@gmail.com>
2021-11-07 21:30:51 -0300

    feat: create index route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+use App\Http\Controllers\UserController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('users', [UserController::class, 'index']);
